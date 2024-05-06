7ee03fbdaf7beec0af32bcde4a975283951ed071
hfdz1011 <18576722857@163.com>
2024-05-06 15:06:05 +0800

    1

--- wstShare/Classes/LXStaticManger.swift
+++ wstShare/Classes/LXStaticManger.swift
@@ -11,23 +11,3 @@ class LXStaticManger: NSObject {
 
 }
 
-struct Color {
-    
-    
-}
-
-struct Font {
-    
-    static func pfRegular(ofSize size:CGFloat) -> UIFont{
-        return UIFont(name: "PingFangSC-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
-    }
-    
-    static func pfMedium(ofSize size:CGFloat) -> UIFont{
-        return UIFont(name: "PingFangSC-Medium", size: size) ?? UIFont.systemFont(ofSize: size)
-    }
-    
-    static func pfSemibold(ofSize size:CGFloat) -> UIFont{
-        return UIFont(name: "PingFangSC-Semibold", size: size) ?? UIFont.systemFont(ofSize: size)
-    }
-    
-}
